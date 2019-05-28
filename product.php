befc468aca7dec7191b0e904167f80a550785d5d
Jay Usadadiya <jusadadiya2356@conestogac.on.ca>
2019-05-28 12:30:38 -0400

    product login element

--- product.php
+++ product.php
@@ -1,49 +1,55 @@
 <?php
   session_start();
-  $post = trim(file_get_contents("php://input"));
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+  $resp = new stdClass();
+    $post = trim(file_get_contents("php://input"));
 	//and decode it into an associative array
 	$json = json_decode($post, true);
-   
-  try {
 	//get the raw POST content 
-  
-	 
-    if(isset($json['email']) && isset($json['password']))
-    {
 	$success = false;
 	$message = '';
-    $dbuser="root";
-	 $email = $json['email'];
-	$password = $json['password'];
-	  
-    // Create connection to mySQL server
-    $db = new PDO("mysql:dbname=sportdb;host=localhost,$dbuser"); 
-    // set the PDO error mode to exception
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $database_hostname = "localhost";
+	$database_user = "root";
+	$database_password = "";
+	$database_name = "sportdb";
+    try{
+		$db = new PDO("mysql:host=$database_hostname;dbname=$database_name",$database_user,$database_password);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+	}catch(PDOException $z){
+ 
+		die($z->getMessage());
+	}
 	
 	$sql = $db->prepare("SELECT * FROM user WHERE userEmail = :email");
-	$sql->bindValue(':email', $email);
+	$sql->bindValue(':email', $json['email']);
 	$sql->execute();
 	
-	if($user = $sql->fetch(PDO::FETCH_ASSOC)){
-		if (password_verify($password, $user['userPassword'])){
+	if($user = $sql->fetch(PDO::FETCH_ASSOC))
+    {
+		if (password_verify($json['password'], $user['userPassword']))
+        {
 			$success = true;
 			$_SESSION['user'] = $user['uid'];
-		} else
+		} 
+        else
+        {
 			$message = 'Password does not match';
-	} else
+	    }
+    } 
+    else
+    {
 		$message = 'Email does not match our records';
+    }
 	
-	$resp = new stdClass();
 	$resp->success = $success;
 	$resp->message = $message;
 
 	echo json_encode($resp);
-      }
-  } catch(PDOException $e) {
-	  print $e->getMessage();
-  }
-
+     
+}
     $database_hostname = "localhost";
 	$database_user = "root";
 	$database_password = "";
