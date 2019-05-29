5244d4862ceb180b0a5cff94ced614d8b5032565
Jay Usadadiya <jusadadiya2356@conestogac.on.ca>
2019-05-29 00:44:08 -0400

    modify user account details

--- registration.php
+++ registration.php
@@ -1,59 +1,4 @@
 <?php
-/*$username = "root";
-$password = "";
-$connected = false;
-
-try {
-    #connect to the database
-    $db = new PDO('mysql:dbname=sportdb; host=localhost',$username, $password); 
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $connected = true;
-} catch(PDOException $e) {
-    die($e->getMessage());
-    #log the exception to a file on the server side
-}
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
- $resp = new stdClass();
- $resp->id = 0;
-   
-        try{
-            $post = trim(file_get_contents("php://input"));
-            $json = json_decode($post, true);
-
-            if (isset($json['userEmail']) &&  strlen($json['userEmail'])> 0 && 
-                isset($json['userPassword']) &&
-                strlen($json['userPassword'])> 0 &&
-                isset($json['user_fname']) &&
-                strlen($json['user_fname'])> 0 &&
-                isset($json['user_lname']) &&
-                strlen($json['user_lname'])> 0 &&
-                isset($json['userName']) && 
-                strlen($json['userName'])> 0 &&
-                isset($json['userShippingAdd']) &&
-                strlen($json['userShippingAdd'])> 0){
-                echo var_dump($json);
-                $cmd = 'INSERT INTO user (userEmail,userPassword,user_fname,user_lname,userName,userShippingAdd)' .
-                'VALUES (:email,:password,:fname,:lname,:username,:ship)';
-                $sql = $db->prepare($cmd);
-                $sql->bindValue(':email', $json['userEmail']);
-                $sql->bindValue(':password', $json['userPassword']);
-                $sql->bindValue(':fname', $json['user_fname']);
-                $sql->bindValue(':lname', $json['user_lname']);
-                $sql->bindValue(':username', $json['userName']);
-                $sql->bindValue(':ship', $json['userShippingAdd']);
-           
-                $sql->execute();
-                $resp->id = $db->lastInsertId();
-            }
-        } catch(Exception $e) {
-            die($e->getMessage());
-        }
-    
-    echo json_encode($resp);
-}*/
-    
     
   //get the json the client sent
   $post = trim(file_get_contents("php://input"));
@@ -69,7 +14,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
   $useraddress= $json['ShippingAdd'];
   $hash = password_hash($password, PASSWORD_DEFAULT);
 
-  if (isset($email) && isset($password)  && isset($fname)  && isset($lname)  && isset($username)  && isset($useraddress)){
+  if (isset($email) && isset($password)  && isset($fname)  && isset($lname)  && isset($username)  && isset($useraddress))
+  {
     //open access to our database
     try {
         $db = new PDO('mysql:host=localhost; dbname=sportdb;', 'root', ''); 
@@ -81,7 +27,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
         
         $user = $sql->fetch(PDO::FETCH_ASSOC);
 
-        if (!isset($user['uid'])){
+        if (isset($user['userEmail']))
+        {
+            //
+            $cmd1 = 'Update user SET userEmail=:email,userPassword=:password,user_fname=:fname,user_lname=:lname,userName=:username,userShippingAdd=:useraddress where uid=:uid';
+            $sqls = $db->prepare($cmd1);
+            $sqls->bindValue(':uid', $user['uid']);
+            $sqls->bindValue(':email', $email);
+            $sqls->bindValue(':password', $hash);
+             $sqls->bindValue(':fname', $fname);
+            $sqls->bindValue(':lname', $lname);
+             $sqls->bindValue(':username', $username);
+            $sqls->bindValue(':useraddress', $useraddress);
+            $sqls->execute();
+            $success = true;
+            
+        } 
+        else 
+        {
             //if not, create a new user record and save it
             $cmd = 'INSERT INTO user (userEmail, userPassword,user_fname,user_lname,userName,userShippingAdd)' .
                 'VALUES (:email, :password,:fname,:lname,:username,:useraddress)';
@@ -94,15 +57,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
             $sql->bindValue(':useraddress', $useraddress);
             $sql->execute();
             $success = true;
-        } else
-            $message = 'user ' . $user['uid'] . ' found';
-    } catch(PDOException $e) {
+           
+        }
+        
+    } 
+      catch(PDOException $e) 
+    {
         $message = $e->getMessage() . ' cannot connect to database';
-    } catch(Exception $e){
-        $message = $e->getMessage() . ' something else went wrong';
-    }
-  } else
-    $message = 'email/password not set';
+    } 
+     
+  } 
+else
+  {
+      $message = 'email/password not set';
+  }
+    
 
   //report back to client
   $myObj = new stdClass();
