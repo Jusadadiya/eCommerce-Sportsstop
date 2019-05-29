044cdde8b54bcddeaea8e33b74001ec3879f4ebb
Himani <hpatel8468@conestogac.on.ca>
2019-05-28 23:49:21 -0400

    comment page login element

--- comment.php
+++ comment.php
@@ -1,19 +1,58 @@
 <?php
-$database_hostname = "localhost";
-$database_user = "root";
-$database_password = "";
-$database_name = "sportdb";
+    session_start();
+
+    $post = trim(file_get_contents("php://input"));
+	//and decode it into an associative array
+	$json = json_decode($post, true);
+    //get the raw POST content 
+    $success = false;
+	$message = '';
+    $database_hostname = "localhost";
+    $database_user = "root";
+    $database_password = "";
+    $database_name = "sportdb";
 
 try{
-    $database_connection = new PDO("mysql:host=$database_hostname;dbname=$database_name",$database_user,$database_password);
-    $database_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db = new PDO("mysql:host=$database_hostname;dbname=$database_name",$database_user,$database_password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 }catch(PDOException $z){
 
     die($z->getMessage());
 }
-$query = "SELECT product_Id,userEmail,productRating, product_comment FROM comments";	
-$statement = $database_connection->prepare($query);
+ if(isset($json['email']) &&isset($json['password']))
+          {
+
+                $sql = $db->prepare("SELECT * FROM user WHERE userEmail = :email");
+                $sql->bindValue(':email', $json['email']);
+                $sql->execute();
+
+                if($user = $sql->fetch(PDO::FETCH_ASSOC))
+                    {
+                        if (password_verify($json['password'], $user['userPassword']))
+                        {
+                            $success = true;
+                            $_SESSION['user'] = $user['uid'];
+                             $message = 'Password match';
+                        } 
+                        else
+                        {
+                            $message = 'Password does not match';
+                        }
+                    } 
+                else
+                    {
+                        $message = 'Email does not match our records';
+                    }
+                 $resp = new stdClass();
+                $resp->success = $success;
+                $resp->message = $message;
+
+                echo json_encode($resp);
+
+        }
+$query = "SELECT * FROM comments";	
+$statement = $db->prepare($query);
 $statement->execute();
 
 $userData_List = array();
